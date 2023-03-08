b769604a989632673b0eae16ba4a14de8d3ccdd4
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-03-08 18:58:02 -0300

    Created a style file for Pet component

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -1,6 +1,8 @@
+import styles from './Pet.module.css'
+
 export default function Pet({name, animal, breed}) {
   return (
-    <div>
+    <div className={styles.pet}>
       <h2>{name}</h2>
       <h3>{animal}</h3>
       <h3>{breed}</h3>
