136b2284c1c64257f599a805354a153539e8af0a
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-03-08 18:55:04 -0300

    Pet component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Pet from './components/Pet'
 
 function App() {
 
   return (
     <div className="App">
       <h1>React</h1>
+      <Pet name="Lolla" animal="Dog" breed="Yorkshire"/>
+      <Pet name="Bidu" animal="Dog" breed="Yorkshire"/>
+      <Pet name="Toby" animal="Dog" breed="Yorkshire"/>
     </div>
   )
 }
---
+++ src/components/Pet.jsx
@@ -0,0 +1,9 @@
+export default function Pet({name, animal, breed}) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <h3>{animal}</h3>
+      <h3>{breed}</h3>
+    </div>
+  )
+}
