adbf0dba66fd144d9bd596728244cc32b86203c4
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-03-21 18:48:12 -0300

    Component composition

--- src/components/Pet/index.jsx
+++ src/components/Pet/index.jsx
@@ -1,11 +1,21 @@
-import styles from './Pet.module.css'
+import styles from "./Pet.module.css";
 
-export default function Pet({name, animal, breed}) {
+export default function Pet({ name, animal, breed, images, location, id }) {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
   return (
-    <div className={styles.pet}>
-      <h2>{name}</h2>
-      <h3>{animal}</h3>
-      <h3>{breed}</h3>
-    </div>
-  )
+    <a href={`/details/${id}`} className={styles.pet}>
+      <div className="image-container">
+        <img src={hero} alt={name} className={styles.thumb} />
+      </div>
+      <div className="info">
+        <h2>{name}</h2>
+        <h3>
+          {animal} - {breed} - {location}
+        </h3>
+      </div>
+    </a>
+  );
 }
---
+++ src/components/Results/index.jsx
@@ -0,0 +1,27 @@
+import Pet from "../Pet";
+import styles from "./Results.module.css";
+
+const Results = ({ pets }) => {
+  return (
+    <div className={styles.results}>
+      {!pets.length ? (
+        <h2>No Pets Found</h2>
+      ) : (
+        pets.map((pet) => {
+          return (
+            <Pet
+              name={pet.name}
+              breed={pet.breed}
+              animal={pet.animal}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              key={pet.id}
+            />
+          );
+        })
+      )}
+    </div>
+  );
+};
+
+export default Results;
