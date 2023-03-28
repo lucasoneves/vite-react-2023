e25181ff2c0921bfc4b16e41c6964a1b08ee7798
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-03-28 19:39:28 -0300

    react query configuration

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,37 @@ import SearchParams from "./components/SearchParams";
 import { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <header className="container">
-          <Link to="/">adoção de pets</Link>
-        </header>
-        <div className="App">
-          {/* <div className="container">
+        <QueryClientProvider client={queryClient}>
+          <header className="container">
+            <Link to="/">adoção de pets</Link>
+          </header>
+          <div className="App">
+            {/* <div className="container">
             <SearchParams />
           </div> */}
-          <div className="container">
-            <Routes>
-              <Route path="/details/:id" element={<Details />}></Route>
-              <Route path="/" element={<SearchParams />}></Route>
-            </Routes>
+            <div className="container">
+              <Routes>
+                <Route path="/details/:id" element={<Details />}></Route>
+                <Route path="/" element={<SearchParams />}></Route>
+              </Routes>
+            </div>
           </div>
-        </div>
+        </QueryClientProvider>
       </BrowserRouter>
     </>
   );
